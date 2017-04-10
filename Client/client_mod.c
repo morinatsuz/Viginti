9c703345b90eb640bcf9ba196995f6f9e399d980
tonkung.io <justminemind@gmail.com>
2017-04-10 21:02:09 +0700

    Server / Client master code
    Server with oop
    Client non

---
+++ Client/client_mod.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <winsock2.h>
+
+#define CMD_HOST 0x03
+
+
+
+void home_start()
+{
+    printf("\n");
+    printf(" ####################################################################\n");
+    printf(" ##  ____    ____      _______        ___    __   _________        ##\n");
+    printf(" ##  \\   \\  /   / __  | ______|  __  |   \\  |  | |         |  __   ##\n");
+    printf(" ##   \\   \\/   / |  | | | ____  |  | |    \\ |  | |__     __| |  |  ##\n");
+    printf(" ##    \\      /  |  | | | |__ | |  | |  |\\ \\|  |    |   |    |  |  ##\n");
+    printf(" ##     \\    /   |  | | |___| | |  | |  | \\    |    |   |    |  |  ##\n");
+    printf(" ##      \\__/    |__| |_______| |__| |__|  \\___|    |___|    |__|  ##\n");
+    printf(" ##                                                                ##\n");
+    printf(" ####################################################################\n\n");
+    printf(" Viginti [Multiplayer Version] - Client Module 0.1A (Debug build)\n\n");
+    printf("[Client Module Initializing...]\n\n");
+}
+
+int sock_start()
+{
+     //This process was to intialize winsock module using WSADATA and create a socket
+
+    SOCKET soc;
+    struct sockaddr_in ser;
+
+    printf("- Initialize Winsock 2.2...");
+
+    WSADATA w;
+	int error = WSAStartup(0x0202,&w);
+
+	if (error)
+	{
+		printf("Error:  You need WinSock 2.2!\n");
+		return 0;
+	}
+
+	printf(" [PASS]\n");
+
+	if (w.wVersion!=0x0202)
+	{
+		printf("Error:  Wrong WinSock version!\n");
+		WSACleanup();
+		return 0;
+	}
+
+	soc = socket (AF_INET,SOCK_STREAM,0);
+
+	printf("- Connecting to server...");
+
+    ser.sin_family = AF_INET;
+    ser.sin_port = htons (5555);	// RPSS port is 5555
+    ser.sin_addr.s_addr = inet_addr ("127.0.0.1");
+
+    if (connect(soc, (LPSOCKADDR)&ser, sizeof(ser))==SOCKET_ERROR)
+    {
+        printf(" [ERROR: Can't connect to server!]\n\n");
+        WSACleanup();
+
+        printf("----------------------------------------\n");
+        printf("\nPlease restart and try again\n\n");
+        printf("[Press any key to exit]");
+        getch();
+        exit(0);
+        return 0;
+    }
+
+    printf(" [PASS]\n\n");
+    printf("[Server successfully connect! - Starting game...]\n\n");
+    printf("----------------------------------------\n\n");
+
+    char pname[20];
+    char cmd[2];
+    char max_player[20];
+
+    while(recv(soc,cmd,2,0) > 0)
+    {
+        if(cmd[0] == CMD_HOST)
+        {
+            printf("You're host player!...Creating lobby\n");
+            printf("Please configure lobby setting\n\n");
+
+            printf("[How many player in this game? (2-4 Person)] \n: ");
+            int sum_players = 0;
+            scanf("%d", &sum_players);
+            while(sum_players <= 1 || sum_players > 4)
+                {
+                    printf("\n###Number of player is not correct###\n");
+                    printf("Please try again enter  (2 to 4 players) : ");
+                    scanf("%d", &sum_players);
+                }
+
+            int sum_net = htonl(sum_players);
+            send(soc, (const char*)&sum_net, sizeof(sum_players),0);
+
+            printf("\n[Please enter your game ending number] \n: ");
+            int max_num = 0;
+            scanf("%d", &max_num);
+            while(max_num < 20)
+                {
+                    printf("\n###Number over is not correct###\n");
+                    printf("Please try again enter Num over (num >= 20) : ");
+                    scanf("%d", &max_num);
+                }
+
+            int max_net = htonl(max_num);
+            send(soc, (const char*)&max_net, sizeof(max_num), 0);
+
+            printf("[Lobby successfully configured!] \n\n");
+            printf("[Player 1, Please enter your name] \n: ");
+            char name[20];
+            scanf("%s", name);
+            printf("\n\nHello %s!, Welcome to Viginti\n", name);
+            printf("[Please wait for other player...]");
+            send(soc, name, sizeof(name), 0);
+        }
+    }
+
+
+}
+
+int main()
+{
+    home_start();
+    sock_start();
+    getch();
+
+}
+
---
+++ Server/server_oop.c
@@ -0,0 +1,137 @@
+//VIGINTI SERVER MODULE//
+//Version 0.1a//
+
+#include <winsock2.h>
+#include <stdio.h>
+#include <windows.h>
+
+//Define section for short command with server//
+#define CMD_HOST 0x03
+//Main Code//
+
+//Global Variable//
+struct sockaddr_in ser; //Struct of server information
+struct sockaddr cli[4]; //Struct of client information
+int addr_size = sizeof (struct sockaddr); //Size of address depend on player = 4
+int num_players = 0; //this int use to indicate player total numbers
+SOCKET soc[5];
+
+int main(){
+
+    socketstart();
+    header();
+    hostmode();
+    getch();
+
+}
+
+void header(){
+
+ //Welcome Message//
+    printf("[                      Viginti Server Module                     ]\n");
+    printf("[        Version 0.1A  Developed by Viginti Development Team     ]\n\n\n");
+    printf("As part of Computer Programming Project 2017\n\n");
+    printf("Faculty of Information Technology\n");
+    printf("King Mongkut's Institute of Technology Ladkrabang\n\n");
+	printf("----------------------------------\n\n");
+
+}
+
+int hostmode(){
+	printf("Waiting for Host Player........");
+
+	while(num_players != 1){
+		soc[num_players+1] = accept(soc[0],&cli[num_players],&addr_size);
+
+		if (soc[num_players+1]==INVALID_SOCKET)
+		{
+			printf("Error:  Unable to accept connection!\n");
+			WSACleanup ();
+			return 0;
+		}
+		else
+		{
+			printf("Host Player joined!\n\n");
+			char cmd[2];
+			sprintf(cmd,"%c%d",CMD_HOST,0);
+			send(soc[1],cmd,2,0);
+			num_players++;
+
+			//room setting progress
+			int sum_net;
+			int max_player;
+			int maxnum_net;
+			int maxnum;
+			printf("[Lobby Setting]\n\n");
+            while(recv(soc[1], &sum_net, sizeof(sum_net), 0) > 0)
+            {
+                max_player = ntohl(sum_net);
+                printf("- Player required: %d\n", max_player);
+                break;
+            }
+            while(recv(soc[1], &maxnum_net, sizeof(maxnum_net), 0) > 0)
+            {
+                maxnum = ntohl(maxnum_net);
+                printf("- Ending number: %d\n", maxnum);
+                break;
+            }
+        }
+	}
+}
+
+int socketstart(){
+    //Socket of server[0] + client[4]
+
+	printf("\n\n[Viginti Server Module Initializing...]\n\n");
+
+	WSADATA w; //Initialize Winsock Libraly
+
+	printf("- Initialize Winsock 2.2...");
+
+	int error = WSAStartup(0x0202,&w);
+
+	if (error)
+	{
+		printf(" [ERROR: You need WinSock 2.2!]\n");
+		return 0;
+	}
+
+	printf(" [PASS]\n");
+
+	if (w.wVersion!=0x0202)
+	{
+		printf(" [ERROR:  Wrong WinSock version!]\n");
+		WSACleanup();
+		return 0;
+	}
+
+    //Listening Socket Structure
+    soc[0] = socket (AF_INET,SOCK_STREAM,0);
+	ser.sin_family = AF_INET;
+	ser.sin_port = htons (5555);
+	ser.sin_addr.s_addr = htonl (INADDR_ANY);
+
+	//Server socket binding
+	printf("- Binding Socket...");
+	if (bind(soc[0],(LPSOCKADDR)&ser,sizeof(ser))==SOCKET_ERROR)
+	{
+		printf("Error:  Unable to bind socket!\n");
+		WSACleanup ();
+		return 0;
+	}
+	printf(" [PASS]\n");
+
+    //Server socket listening
+	printf("- Listening Check...");
+	if (listen(soc[0],1)==SOCKET_ERROR)
+	{
+		printf("Error:  Unable to listen!\n");
+		WSACleanup ();
+		return 0;
+	}
+	printf(" [PASS]\n\n");
+	printf("----------------------------------");
+
+    printf("Server Successfully Initialize!\n\n");
+
+    }
